a63c8c3abf061ebc18efa9d32403ed92824beb0a
Piccios <lorenzo.hl.picchi@gmail.com>
2024-07-02 16:34:39 +0200

    set a class Movies filled with three istances

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php 
 class Movies{
+    public $title;
+    public $genre;
+    public $length;
 
+    function __construct ($title, $genre, $length){
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->length = $length;
+    }
 }
 
 
-$Star_Wars = new Movies();
-$Avengers = new Movies();
-?>
+$Star_Wars = new Movies('Star Wars', 'Sci-Fi', 160);
+$Avengers = new Movies("Avengers", "Action", 200);
+$Interstellar = new Movies("Interstellar", "Sci-Fi", 230);
+$TheLegendOfAlJohnAndJack = new Movies("The legend of Al, John & Jack", "Comedy", 100);
+
+
+var_dump($Star_Wars);
+var_dump($Avengers);
+var_dump($Interstellar);   
+var_dump($TheLegendOfAlJohnAndJack);
